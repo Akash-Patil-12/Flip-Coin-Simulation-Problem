06912c78dab55986481858c090ad99aed55ddacd
Akash-Patil-12 <patilakash121212@gmail.com>
2021-07-01 17:08:54 +0530

    Added : Number of times head and tail won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,18 @@
 #!/bin/bash -x
 
-win=$((RANDOM%2))
+headWin=0
+TailWin=0
 
-if (( $win == 1 ))
-then
-   echo "Heads"
-else
-   echo "Tails"
-fi
+for ((i=0;i<20;i++))
+do
+	win=$((RANDOM%2))
+
+	if (( $win == 1 ))
+	then
+   	 ((headWin=$headWin+1))
+	else
+   	 ((TailWin=$TailWin+1))
+	fi
+done
+echo "Head win : $headWin times"
+echo "Tail win : $TailWin times"
