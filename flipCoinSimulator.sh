e53b92615b9fffe23680bd0a5a7356072955f97f
Akash-Patil-12 <patilakash121212@gmail.com>
2021-07-02 15:22:09 +0530

    Added : If its tie then the game continues till difference of minumumof f minimum 2 pints is achieved

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -23,6 +23,33 @@ echo "Tail : $TailWin times"
 if (( $headWin == $TailWin ))
 then
    echo Tie
+   while (( 1 ))
+   do
+    win=$((RANDOM%2))
+
+    if (( $win == 1 ))
+    then
+       ((headWin=$headWin+1))
+    else
+       ((TailWin=$TailWin+1))
+    fi
+    if (( $(($TailWin-$headWin)) >=2 ))
+    then
+      break
+    elif (( $(($headWin-$TailWin)) >=2 ))
+    then
+      break
+    fi
+
+   done
+   if (( $headWin > $TailWin ))
+   then
+      echo "Head Win by $(($headWin-$TailWin)) counts"
+   elif (($TailWin > $headWin ))
+   then
+      echo "Tail win by $(($TailWin-$headWin)) counts"
+   fi
+
 else
    echo Win
 	if (( $headWin > $TailWin ))
